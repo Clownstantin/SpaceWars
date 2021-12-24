c0290e4090cc8339726715776677dea6ee89e362
Clownstantin <constantin2033@gmail.com>
2021-12-24 20:14:55 +0300

    PlayerMovement Update
    Clamped X and Y player position. Added bounds with offset via camera method.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 10f;
-    [SerializeField] private Vector2 _maxBounds = new Vector2(4f, 7.5f);
-    [SerializeField] private Vector2 _minBounds = new Vector2(-4f, -7.5f);
+    [SerializeField] private float _boundOffset = 0.5f;
 
     private Transform _transform;
+    private Camera _camera;
+    private Vector2 _maxXY;
+    private Vector2 _minXY;
 
     private readonly string _horizontalAxis = "Horizontal";
     private readonly string _verticalAxis = "Vertical";
@@ -14,21 +16,35 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _transform = transform;
+        SetUpMoveBoundaries();
     }
+
 
     private void Update()
     {
         Move();
     }
 
+    private void SetUpMoveBoundaries()
+    {
+        _camera = Camera.main;
+
+        Vector3 bounds = new Vector3(_boundOffset, _boundOffset);
+
+        _minXY = _camera.ViewportToWorldPoint(new Vector3(0, 0)) + bounds;
+        _maxXY = _camera.ViewportToWorldPoint(new Vector3(1, 1)) - bounds;
+    }
+
     private void Move()
     {
-        var horizontalMove = Input.GetAxis(_horizontalAxis) * _moveSpeed * Time.deltaTime;
-        var verticalMove = Input.GetAxis(_verticalAxis) * _moveSpeed * Time.deltaTime;
+        Vector3 move = _transform.position;
 
-        var newXPosClamped = Mathf.Clamp(_transform.position.x + horizontalMove, _minBounds.x, _maxBounds.x);
-        var newYPosClamped = Mathf.Clamp(_transform.position.y + verticalMove, _minBounds.y, _maxBounds.y);
+        move.x += Input.GetAxis(_horizontalAxis) * _moveSpeed * Time.deltaTime;
+        move.y += Input.GetAxis(_verticalAxis) * _moveSpeed * Time.deltaTime;
 
-        _transform.position = new Vector2(newXPosClamped, newYPosClamped);
+        move.x = Mathf.Clamp(move.x, _minXY.x, _maxXY.x);
+        move.y = Mathf.Clamp(move.y, _minXY.y, _maxXY.y);
+
+        _transform.position = move;
     }
 }
